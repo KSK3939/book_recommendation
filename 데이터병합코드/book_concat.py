730fcdf7871926133eb499e3fa97ca987f22b839
kimyujeong <kkgt0217@gmail.com>
2026-06-12 12:27:51 +0900

    ㅇㄴㅁ

--- 데이터병합코드/book_concat.py
+++ 데이터병합코드/book_concat.py
@@ -2,18 +2,18 @@ from codecs import ignore_errors
 
 import pandas as pd
 
-df = pd.read_csv('data_나라/yes24_steadyseller_영미.csv')
+df = pd.read_csv('../data_나라/yes24_steadyseller_영미.csv')
 print(df.head())
 
 
 df['국가'] = '영미'
 
-df_temp = pd.read_csv('data_나라/yes24_steadyseller_일본.csv')
+df_temp = pd.read_csv('../data_나라/yes24_steadyseller_일본.csv')
 df_temp['국가'] = '일본'
 df=pd.concat([df_temp, df],ignore_index=True)
 
 
-df_temp = pd.read_csv('data_나라/yes24_steadyseller_한국.csv')
+df_temp = pd.read_csv('../data_나라/yes24_steadyseller_한국.csv')
 df_temp['국가'] = '한국'
 
 df=pd.concat([df_temp, df],ignore_index=True)
@@ -25,17 +25,17 @@ df = df[['ISBN','국가']]
 df['ISBN'] = df['ISBN'].astype('Int64')
 
 
-df_temp = pd.read_csv('data_나라/yes24_steadyseller_독일_정리.csv')
+df_temp = pd.read_csv('../data_나라/yes24_steadyseller_독일_정리.csv')
 df=pd.concat([df_temp, df],ignore_index=True)
 
 
-df_temp = pd.read_csv('data_나라/yes24_steadyseller_중국_정리.csv')
+df_temp = pd.read_csv('../data_나라/yes24_steadyseller_중국_정리.csv')
 df=pd.concat([df_temp, df],ignore_index=True)
 
-df_temp = pd.read_csv('data_나라/yes24_steadyseller_러시아_정리.csv')
+df_temp = pd.read_csv('../data_나라/yes24_steadyseller_러시아_정리.csv')
 df=pd.concat([df_temp, df],ignore_index=True)
 
-df_temp = pd.read_csv('data_나라/yes24_steadyseller_프랑스_정리.csv')
+df_temp = pd.read_csv('../data_나라/yes24_steadyseller_프랑스_정리.csv')
 df=pd.concat([df_temp, df],ignore_index=True)
 
 print(df.head())
